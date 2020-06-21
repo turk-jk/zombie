c9b0c06a367ec695895dd9d8006f20d8097e3715
turk-jk <jacob.kazal@gmail.com>
2020-06-21 16:44:15 +0800

    Initial Commit

---
+++ zombie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zombie
+//
+//  Created by yacob jamal kazal on 21/6/20.
+//  Copyright © 2020 yacob jamal kazal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
